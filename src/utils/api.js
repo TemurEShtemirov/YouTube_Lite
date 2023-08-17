2b1c39f2c1700841f9f32b6adc6c72f3fdd5e2ac
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-08-18 00:22:09 +0500

    done

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "58288092a0mshc63ac45cd9447a7p1f6a03jsn619a2396bb62",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
